7b50ae2eb3f096fa2e876ad5c38583d1acf085e6
Muhammed Salman <m.salman0320@gmail.com>
2023-04-18 15:25:42 +0200

    css styled create page

--- src/Form/MealType.php
+++ src/Form/MealType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Meal;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,14 +14,21 @@ class MealType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        
         $builder
-            ->add('name')
-            ->add('picture')
-            ->add('category')
-            ->add('calories')
-            ->add('rating')
-            ->add('preparation')
-            ->add('cooking_time')
+            ->add('name', null, array("attr" => ["class" => "form-control w-25", "placeholder" => "Write the Name of the Meal"]))
+            ->add('picture', FileType::class, array("attr" => ["class" => "form-control w-25", "placeholder" => "Write the Name of the Event"]))
+            ->add('category', ChoiceType::class, array("attr" => ["class" => "form-control w-25", "placeholder" => "Write the Name of the Event"], 'choices' =>[
+                '' => null,
+                'Vegie' => "Vegie",
+                'Vegan' => "Vegan",
+                'Meat' => "Meat",
+                'Healthy' => "Healthy"
+                ]))
+            ->add('calories', null, array("attr" => ["class" => "form-control w-25", "placeholder" => "Write the Name of the Event"]))
+            ->add('rating', null, array("attr" => ["class" => "form-control w-25", "placeholder" => "Write the Name of the Event"]))
+            ->add('preparation', TextareaType::class, array("attr" => ["class" => "form-control w-25", "placeholder" => "Write the Name of the Event"]))
+            ->add('cooking_time', null, array("attr" => ["class" => "form-control w-25", "placeholder" => "Write the Name of the Event"]))
         ;
     }
 
