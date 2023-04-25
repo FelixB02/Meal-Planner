1239264f4bd0e010218c3b7bf4d238ba00acd14c
FelixB02 <felix.blechl@chello.at>
2023-04-25 10:16:29 +0200

    adding weeks

--- src/Controller/WeekController.php
+++ src/Controller/WeekController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Week;
 use App\Form\WeekType;
+use App\Repository\MealRepository;
 use App\Repository\WeekRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,66 +14,60 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/week')]
 class WeekController extends AbstractController
 {
-    #[Route('/', name: 'app_week_index', methods: ['GET'])]
+    #[Route('/{id}&{day}', name: 'week_index_set', methods: ['GET'])]
+    public function indexset(WeekRepository $weekRepository, $id, MealRepository $mealRepository, $day): Response
+    {
+        if($week = $weekRepository->findOneBy(['fk_user' => $this->getUser()])) {
+        } else {
+            $week = new Week();
+        }
+        
+        $meal = $mealRepository->find($id);
+        if ($day == 1) {
+            $week->setFkMonday($meal);
+        } elseif($day == 2) {
+            $week->setFkTuesday($meal);
+        } elseif($day == 3) {
+            $week->setFkWednesday($meal);
+        } elseif($day == 4) {
+            $week->setFkThursday($meal);
+        } elseif($day == 5) {
+            $week->setFkFriday($meal);
+        } elseif($day == 6) {
+            $week->setFkSaturday($meal);
+        } elseif($day == 7) {
+            $week->setFkSunday($meal);
+        }
+        $week->setFkUser($this->getUser());
+        $weekRepository->save($week, true);
+
+        return $this->redirectToRoute('app_meal_user_show', ['id' => $id
+        ]);
+    }
+
+    #[Route('/', name: 'week_index', methods: ['GET'])]
     public function index(WeekRepository $weekRepository): Response
     {
+        $week = $weekRepository->findOneBy(['fk_user' => $this->getUser()]);
+        $week->setFkUser($this->getUser());
+        $weekRepository->save($week, true);
+        $monday = $week->getFkMonday();
+        $tuesday = $week->getFkTuesday();
+        $wednesday = $week->getFkWednesday();
+        $thursday = $week->getFkThursday();
+        $friday = $week->getFkFriday();
+        $saturday = $week->getFkSaturday();
+        $sunday = $week->getFkSunday();
+
         return $this->render('week/index.html.twig', [
-            'weeks' => $weekRepository->findAll(),
+            'weeks' => $weekRepository->findBy(['fk_user' => $this->getUser()]),
+            'monday' => $monday,
+            'tuesday' => $tuesday,
+            'wednesday' => $wednesday,
+            'thursday' => $thursday,
+            'friday' => $friday,
+            'saturday' => $saturday,
+            'sunday' => $sunday,
         ]);
-    }
-
-    #[Route('/new', name: 'app_week_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, WeekRepository $weekRepository): Response
-    {
-        $week = new Week();
-        $form = $this->createForm(WeekType::class, $week);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $weekRepository->save($week, true);
-
-            return $this->redirectToRoute('app_week_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('week/new.html.twig', [
-            'week' => $week,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_week_show', methods: ['GET'])]
-    public function show(Week $week): Response
-    {
-        return $this->render('week/show.html.twig', [
-            'week' => $week,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_week_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Week $week, WeekRepository $weekRepository): Response
-    {
-        $form = $this->createForm(WeekType::class, $week);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $weekRepository->save($week, true);
-
-            return $this->redirectToRoute('app_week_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('week/edit.html.twig', [
-            'week' => $week,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_week_delete', methods: ['POST'])]
-    public function delete(Request $request, Week $week, WeekRepository $weekRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$week->getId(), $request->request->get('_token'))) {
-            $weekRepository->remove($week, true);
-        }
-
-        return $this->redirectToRoute('app_week_index', [], Response::HTTP_SEE_OTHER);
     }
 }
